4a87eaaba48662e51e81b7c4d81c12a76f942604
yuliyambg <yuliyambg@gmail.com>
2021-02-07 19:14:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, holiday) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${holiday} gift!`;
+    }
+    return array;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
